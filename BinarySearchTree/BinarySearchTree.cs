bfb59c133bec1894de86fd731952d12b91eca52c
Courtney Severson <courtneyseverson8@gmail.com>
2018-03-06 20:40:32 -0600

    added methods to add to tree and search

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        public int data;
+        public int node;
+        public BinarySearchTree left;
+        public BinarySearchTree right;
+
+        public BinarySearchTree(int data)
+        {
+            this.data = data;
+        }
+        public bool ExistingNumber()
+        {
+            return (right == null && left == null);
+        }
+        public void Loop()
+        {
+            //need to use a while loop
+            //need to show what happens for first value null == 0 (if else)
+            //need to use else to show what happens right or left
+        }
+        public void AddMethod(int data)
+        { if (node == 0)
+            {
+                if (right == null)
+                {
+                    right = new BinarySearchTree(data);
+                }
+                else if (left == null)
+                {
+                    left = new BinarySearchTree(data);
+                }
+            }
+           else if (node > 0)
+            {
+                if (left)
+                {
+                    left.AddMethod(data);
+                }
+                else if (right)
+                {
+                    right.AddMethod(data);
+                }
+            }
+
+        }
+        public void SearchMethod()
+        {
+            
+        }
+        public void DisplaySearchMethod()
+        {
+            if (left != null)
+            {
+                Console.WriteLine(data);
+            }
+            if (right != null)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BinarySearchTree test = new BinarySearchTree();
+
+            //test.Add(5);
+            //add more numbers to test
+
+        }
+    }
+}
